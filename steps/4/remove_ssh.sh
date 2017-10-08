0316ea5b43c4e3bcfc92447b645ad6a5735244da
Fernando Ripoll <pipo02mix@gmail.com>
2017-10-08 22:11:49 +0200

    Add slides and some fixes v2

---
+++ steps/4/remove_ssh.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+i="0"
+while [ $i -lt 2 ]; do
+  PRO=$(ps -alef | grep "ssh -f" | awk '{ print $2;}')
+  if [ "$PRO" ]
+  then
+    echo "ssh process corrected ${PRO}"
+    kill -9 $PRO &> /dev/null 
+    sleep 5
+  fi
+  i=$[$i+1]
+done  
+CLUSTER_IP="$(kubectl get po -o wide | grep back | awk '{ print $6; }' | head -1)"
+ssh -f -o ExitOnForwardFailure=yes core@172.17.4.202 -L 8081:$CLUSTER_IP:8081 sleep 10
