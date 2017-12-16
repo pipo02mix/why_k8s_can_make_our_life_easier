9427fcd5d8fc1247ebd295ca21445ef07bd4835b
Fernando Ripoll <pipo02mix@gmail.com>
2017-12-16 16:38:17 +0100

    Fix some kubeadm new stuff

--- cluster/ubuntu/node.sh
+++ cluster/ubuntu/node.sh
@@ -1,4 +1,4 @@
 #Script to add the node to the cluster
 
 kubeadm reset
-kubeadm join --token $KUBETOKEN $MASTER_IP:6443
+kubeadm join --discovery-token-unsafe-skip-ca-verification --token $KUBETOKEN $MASTER_IP:6443
